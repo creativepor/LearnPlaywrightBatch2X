bbe37dc1b9389e8f2c3540424d76d51feb5d44ad
PoulomiRoy <poulomi.partho@gmail.com>
2026-05-13 09:52:21 +0530

    Add Chapter_04_Javascript_Concepts with JavaScript concepts examples

---
+++ Chapter_04_Javascript_Concepts/09_var_let_Const.js
@@ -0,0 +1,32 @@
+var v=10;
+
+var v=20;
+
+console.log(v); // Output: 20
+var browser = "chrome";
+var browser = "firefox"; // redeclaration allowed
+browser = "edge"; // reassignment allowed
+
+// for, functions
+
+var testCases = ["login", "logout", "signup"];
+
+for (var i = 0; i < testCases.length; i++) {
+    console.log("Running test:", testCases[i]);
+}
+
+console.log("Loop counter leaked outside:", i);
+
+
+
+console.log("Hi");
+console.log("Hi");
+console.log("Hi");
+
+function say() {
+    console.log("Hi from Function");
+}
+
+
+say();
+say();
