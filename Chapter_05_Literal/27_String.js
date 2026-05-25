5b40209beb308c5f6a2f5869571a34603c3a9146
PoulomiRoy <poulomi.partho@gmail.com>
2026-05-25 12:32:26 +0530

    Add Chapter_10_Loops folder

--- Chapter_05_Literal/27_String.js
+++ Chapter_05_Literal/27_String.js
@@ -14,3 +14,51 @@ console.log(typeof c1);
 console.log(typeof double);
 
 // 'JavaScript prefers to use single code. '
+
+
+
+
+
+
+
+
+
+
+
+
+function getAPIStatusCode(code) {
+  if (code >= 200 && code <= 299) {
+    return "Success";
+  } else if (code >= 300 && code <= 399) {
+    return "Redirection";
+  } else if (code >= 400 && code <= 499) {
+    return "Client Error";
+  } else if (code >= 500 && code <= 599) {
+    return "Server Error";
+  } else {
+    return "Invalid";
+  }
+}
+
+// Sample usage:
+console.log(getAPIStatusCode(200)); // "Success"
+console.log(getAPIStatusCode(301)); // "Redirection"
+console.log(getAPIStatusCode(404)); // "Client Error"
+console.log(getAPIStatusCode(500)); // "Server Error"
+console.log(getAPIStatusCode(999)); // "Invalid"
+
+
+
+let statusCode = 404;
+
+if (statusCode >= 200 && statusCode < 300) {
+    console.log("Success");
+} else if (statusCode >= 300 && statusCode < 400) {
+    console.log("Redirection");
+} else if (statusCode >= 400 && statusCode < 500) {
+    console.log("Client Error");
+} else if (statusCode >= 500 && statusCode < 600) {
+    console.log("Server Error");
+} else {
+    console.log("Invalid Staus Code");
+}
